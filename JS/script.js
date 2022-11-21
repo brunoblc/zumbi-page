d405ee85b31e7f8138a970e6f6ebeabb687424a7
brunoblc <brunoluiz_2210@hotmail.com>
2022-11-21 19:50:16 -0300

    Novo update

--- JS/script.js
+++ JS/script.js
@@ -44,21 +44,21 @@ iniciaTabNav();
 
 iniciaAccordion();
 
-// const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-// function scrollToSection(event){
-//     event.preventDefault();
-//     const href = event.currentTarget.getAttribute('href');
-//     const section = document.querySelector(href);
-//     console.log(section.offsetTop);
+function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    console.log(section.offsetTop);
 
-//     window.scrollTo(0, 1000);
-// }
+    window.scrollTo(0, 1000);
+}
 
-// linksInternos.forEach((link) =>{
-//     link.addEventListener('click', scrollToSection);    
+linksInternos.forEach((link) =>{
+    link.addEventListener('click', scrollToSection);    
 
-// })
+})
 
 
 
